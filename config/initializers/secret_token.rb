4af1576541db0e6c9343a99a5d7eac174485c2d4
Grace Moss <moss.grace@gmail.com>
2016-01-30 13:58:48 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdeasApp::Application.config.secret_token = '686ccf2d1e3e218350bc9dbd71e67f26333a015e1bb4474eb2b061d5f4a4ed6d1fd44d7fadf19a65f8d26f1314daafed0eec093071f08af4e506dcf0bcb35de8'
